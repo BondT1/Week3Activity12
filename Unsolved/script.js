7d9f80be7b0eb342cb9b4d2355648096380809e7
Tom Bond <tombond248@gmail.com>
2022-10-06 19:14:30 +0100

    complete

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,25 @@
 // WRITE YOUR CODE HERE
+
+var students = ["Tom", "James", "Chloe", "Amy", "Ellie" ]
+
+console.log(students.length);
+
+console.log("welcome to the class " + students [0]);
+console.log("welcome to the class " + students [1]);
+console.log("welcome to the class " + students [2]);
+console.log("welcome to the class " + students [3]);
+console.log("welcome to the class " + students [4]);
+
+students [0] = "Kenny";
+
+if (students[0] === "Kenny") {
+    console.log (students [0] + " is in class")
+}
+
+
+
+
+
+
+
+
